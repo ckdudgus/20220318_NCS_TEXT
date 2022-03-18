a44153882de08160f34bcc3ee3065ed830c24850
ckdudgus <c91878074@gmail.com>
2022-03-18 12:02:54 +0900

    노드서버연동완료

--- routes/getsend.js
+++ routes/getsend.js
@@ -1,9 +1,29 @@
 var express = require('express');
+var mysql = require('mysql');
+var dbconfig = require('../db/config.js')
+const connection =mysql.createConnection(dbconfig);
 var router = express.Router();
 
 router.get('/',(req , res) => {
     res.send('getsend라는 요청의 응답')
 })
-
+router.get('/getjson',(req , res) => {
+    res.send({'url':'/getsend/getjson'})
+})
+router.post('/post',(req , res) => {
+    res.send({
+        'url':'getsend/post ' , 
+        'title':'getsend.js내의  post라우팅'})
+})
+router.get('/awsSQL' , (req ,res) => {
+    connection.query(
+        'select * from react_preinterview.interview',
+        (error , rows) => {
+            if(error) throw error;
+            console.log('해당 스키마만의 테이블내용' , rows);
+            res.send(rows);
+        }
+    )   
+})
 
 module.exports = router;
--- routes/postsend.js
+++ routes/postsend.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/',(req , res) => {
+router.post('/',(req , res) => {
     res.send('postsend라는 요청의 응답')
 })
+
 
 
 module.exports = router;
